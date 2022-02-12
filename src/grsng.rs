7e6bc0ef98cb7bd6a5feecb0c4ec2ed3371c4753
emdash <dotsony@gmail.com>
2022-02-11 18:21:00 -0800

    Towards a general implementation

--- src/grsng.rs
+++ src/grsng.rs
@@ -45,24 +45,20 @@ use crate::SigmaRules;
  * clauses into one place.
  */
 trait Types {
-    type Val: Value;     // Node data
-    type Id: NodeId;     // A way of referring to a data node. Must be copy.
-    type Var: Variable;  // A way of referring to a pattern node. Must be copy.
+    // The data contained within a node.
+    type Val: Debug + Copy + PartialEq + SigmaRules;
+    // Id of nodes in a data graph
+    type Id: Debug + Copy + PartialEq;
+    // Id of nodes in a pattern.
+    type Var: Debug + Copy + PartialEq;
 }
-
-
-// Helper traits. This lets us re-use these bounds in the few places
-// where we can't refer to T::Foo directly.
-trait Value: Debug + Copy + PartialEq + SigmaRules {}
-trait NodeId: Debug + Copy + PartialEq {}
-trait Variable: Debug + Copy + PartialEq {}
 
 
 /**
  * This trait maps vars to IDs for rule rewriting.
  */
 trait Mapping<T: Types>: Debug {
-    fn get(&self, var: T::Var) -> T::Val;
+    fn get(&self, var: T::Var) -> T::Id;
     fn merge(self, other: Self) -> Self;
     fn bind(var: T::Var, id: T::Id) -> Self;
 }
@@ -74,8 +70,10 @@ trait Mapping<T: Types>: Debug {
 trait DataGraph<T: Types> {
     type It: Iterator<Item=T::Id>;
     fn value(&self, id: T::Id) -> T::Val;
-    fn children(&self, id: T::Id) -> Self::It;
-    fn insert(self, id: T::Id, func: T::Val, rest: Self::It) -> Self;
+    fn args(&self, id: T::Id) -> Self::It;
+    fn alloc(&mut self, func: T::Val) -> T::Id;
+    fn append_arg(&mut self, id: T::Id, src: T::Id);
+    fn redirect(&mut self, src: T::Id, dst: T::Id);
     fn root(&self) -> T::Id;
     fn gc(self) -> Self;
 }
@@ -90,14 +88,14 @@ trait Pattern<T: Types> {
 
     fn contains(&self, id: T::Var) -> bool;
     fn value<'a>(&self, id: T::Var) -> T::Val;
-    fn children(&self, id: T::Var) -> Self::It;
-    fn root(&self) -> T::Id;
+    fn args(&self, id: T::Var) -> Self::It;
+    fn root(&self) -> T::Var;
 
     fn matches(
         &self,
         redex: T::Var,
+        data: &impl DataGraph<T>,
         node: T::Id,
-        data: &impl DataGraph<T>
     ) -> Option<Self::Mp> {
         println!("enter: {:?}, {:?}", redex, node);
 
@@ -107,23 +105,40 @@ trait Pattern<T: Types> {
         if redex_value == node_value {
             println!("bind: {:?} -> {:?}", redex, node);
             let mut mapping = Self::Mp::bind(redex, node);
-
-            for (var, id) in self.children(redex).zip(data.children(node)) {
+            let iter = self.args(redex).zip(data.args(node));
+            for (var, id) in iter {
                 println!("bind-rec: {:?}, {:?}", var, id);
                 if self.contains(var) {
-                    mapping = mapping.merge(self.matches(var, id, data)?);
+                    mapping = mapping.merge(self.matches(var, data, id)?);
                 } else {
                     mapping = Self::Mp::bind(var, id);
                 }
                 println!("recurse-done {:?}", mapping);
             }
-
             println!("success: {:?}", mapping);
             Some(mapping)
         } else {
             println!("fail: {:?} != {:?}", redex_value, node_value);
             None
         }
+    }
+
+    fn rewrite(
+        &self,
+        contractum: T::Var,
+        data: &mut impl DataGraph<T>,
+        mapping: &Self::Mp
+    ) -> T::Id {
+        let id = data.alloc(self.value(contractum));
+        for var in self.args(contractum) {
+            if self.contains(var) {
+                let arg_id = self.rewrite(var, data, mapping);
+                data.append_arg(id, arg_id);
+            } else {
+                data.append_arg(id, mapping.get(var))
+            }
+        }
+        id
     }
 }
 
@@ -137,90 +152,35 @@ struct CanonicalRule<T: Types, P: Pattern<T>> {
     redirection: (T::Var, T::Var)
 }
 
-/*
-impl<T: Types> CanonicalRule<T> {
+impl<T: Types, P: Pattern<T>> CanonicalRule<T, P> {
     /**
      * If a rule matches the subgraph rooted at `node`, return the
      * mapping of variables to node ideas..
      */
-    pub fn matches(&self, data: &DataGraph<T>, data_root: T::Id) -> Option<Mapping<T>> {
-        self.matches_rec(self.redex.root(), data, data_root, HashMap::new())
+    pub fn matches(&self, data: &impl DataGraph<T>) -> Option<P::Mp> {
+        self.redex.matches(self.redex.root(), data, data.root())
     }
 
-    // Recursive implementation of above, to avoid forcing client code
-    // to explicitly pass the mapping.
-    fn matches_rec(
-        &self,
-        pattern_root: T::Var,
-        data: &DataGraph<T>,
-        data_root: T::Id,
-        mapping: Mapping<T>
-    ) -> Option<Mapping<T>> {
-        use CanonicalNode as N;
-        use CanonicalTerm::*;
-
-        println!("enter: {:?}, {:?}", pattern_root, data_root);
-
-        let mut mapping = mapping;
-        let N {function: x, id: var, rest: sp} = self.redex.get(pattern_root)?;
-        let N {function: y, id: id,  rest: sd} = data.get(data_root)?;
-
-        println!("check: {:?}, {:?}", x, y);
-
-        if x == y {
-            println!("bind: {:?}, {:?}", *var, *id);
-            mapping.insert(*var, *id);
-            // loop over the rest of the pattern
-            for (pat, node) in sp.iter().zip(sd.iter()) { match (pat, node) {
-                (Id(v), Id(i)) => {
-                    println!("bind-rec: {:?}, {:?}", *v, *i);
-
-                    if let Some(_) = self.redex.get(*v) {
-                        // ensure non-empty sub-terms also match.
-                        // don't bind before-hand, matches will do this for us.
-                        mapping = self.matches_rec(*v, data, *i, mapping)?;
-                    } else {
-                        // just bind the id in the mapping.
-                        mapping.insert(*v, *i);
-                    }
-
-                    println!("recurse-done {:?}", mapping);
-                },
-
-                // Ensure two constructors are the same.
-                (Constructor(x), Constructor(y)) if x == y => (),
-
-                // Early return if any subterms fail to match
-                (x, y) => {
-                    println!("fail: {:?} != {:?}", x, y);
-                    return None
-                }
-                //mapping = matches_term(pat, node, mapping)?;
-            } }
-
-            println!("success: {:?}", mapping);
-            Some(mapping)
+    pub fn rewrite(&self, data: &mut impl DataGraph<T>) -> bool {
+        if let Some(mapping) = self.matches(data) {
+            self.contractum.rewrite(self.contractum.root(), data, &mapping);
+            // XXX: this is an extra step, which ideally we could
+            // avoid by directly writing into the redirection node.
+            //
+            // XXX: not clear we even need redirections given a
+            // functional strategy.
+            data.redirect(
+                mapping.get(self.redirection.0),
+                mapping.get(self.redirection.1)
+            );
+            true
         } else {
-            println!("fail: {:?} != {:?}", x, y);
-            None
+            false
         }
     }
-
-    /*
-    pub fn rewrite(&self, data: DataGraph<T>) -> DataGraph<T> {
-        if let Some(mapping) = self.matches((&data, data.root())) {
-            let rewritten = self.rewrite_rec(data, data.root(), &mapping);
-            data.set(data.root()) 
-        } else {
-            data
-        }
-    }*/
 }
 
-
-struct CanonicalGRS<T: Types>(Vec<CanonicalRule<T>>);
-
-
+/*
 macro_rules! node {
     ($id:expr ; $func:expr) => {
         CanonicalNode {
@@ -252,6 +212,7 @@ macro_rules! rule {
         }
     };
 }
+*/
 
 
 #[cfg(test)]
@@ -271,7 +232,6 @@ mod tests {
     #[derive(Copy, Clone, Debug, PartialEq, Eq)]
     enum Values {Start, Add, If, True, False, Int(i8), Zero, Succ, Hd, Cons}
 
-    // We can punt on sigma rules for now.
     impl SigmaRules for Values {
         type Error = ();
     }
@@ -285,8 +245,8 @@ mod tests {
         type Id  = u8;
     }
 
-    type TestGrs = CanonicalGRS<TestTypes>;
-
+}
+    /*
     #[test]
     fn test_rules() {
         use Symbols::*;
